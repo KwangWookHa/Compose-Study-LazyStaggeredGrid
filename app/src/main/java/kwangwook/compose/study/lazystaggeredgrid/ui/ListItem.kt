b80f95677ea881a808800f8fe1393145277d2f51
Ha Kwang Wook <kwangwookha@icloud.com>
2023-11-19 14:45:34 +0900

    List item 데이터 클래스 정의

---
+++ app/src/main/java/kwangwook/compose/study/lazystaggeredgrid/ui/ListItem.kt
@@ -0,0 +1,9 @@
+package kwangwook.compose.study.lazystaggeredgrid.ui
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+
+data class ListItem(
+    val height: Dp,
+    val color: Color
+)
