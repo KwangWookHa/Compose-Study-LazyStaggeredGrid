c49db7a7bdcbbcad612923f5d646c7c1d9e1b75e
Ha Kwang Wook <kwangwookha@icloud.com>
2023-11-19 14:45:45 +0900

    RandomColorBox 컴포저블 정의

---
+++ app/src/main/java/kwangwook/compose/study/lazystaggeredgrid/ui/RandomColorBox.kt
@@ -0,0 +1,22 @@
+package kwangwook.compose.study.lazystaggeredgrid.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ColorBox(item: ListItem) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(item.height)
+            .clip(RoundedCornerShape(12.dp))
+            .background(item.color)
+    )
+}
